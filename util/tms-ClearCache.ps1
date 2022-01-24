72342decf8baaf43983d47e6e26688eb385c756e
Mihai Filip <36047127+voidthevillain@users.noreply.github.com>
2022-01-24 14:05:21 +0100

    Create tms-ClearCache.ps1

---
+++ util/tms-ClearCache.ps1
@@ -0,0 +1,39 @@
+# Windows
+
+$teams = Get-Process Teams -ErrorAction SilentlyContinue
+$outlook = Get-Process Outlook -ErrorAction SilentlyContinue
+
+# check if Outlook is running and quit it to clear add-in cache
+if ($outlook) {
+  Write-Host 'Outlook is running. Quitting Outlook.'
+  $outlook | Stop-Process
+  Start-Sleep 2
+} else {
+  Write-Host 'Outlook is not running.'
+}
+
+# check if Teams is running and quit it to clear cache
+if ($teams) {
+  Write-Host 'Teams is running. Quitting Teams'
+  $teams | Stop-Process -Force
+  Start-Sleep 2
+} else {
+  Write-Host 'Teams is not running.'
+}
+
+# clear cache
+gci -path $env:AppData\Microsoft\Teams | foreach { Remove-Item $_.FullName -Recurse -Force }
+Write-Host 'Cache cleared.'
+
+# start Outlook
+$toStartOutlook = Read-Host 'Do you wish to start Outlook? [Y/N]'
+if ($toStartOutlook -eq 'Y') {
+  Start-Process Outlook
+}
+
+
+# start Teams
+$toStartTeams = Read-Host 'Do you wish to start Teams? [Y/N]'
+if ($toStartTeams -eq 'Y') {
+  Start-Process -File "$($env:localappdata)\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"'
+}
